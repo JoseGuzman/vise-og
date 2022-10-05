df4f76e911b0eff8c3a60de2b812ecb62ceb8ac8
JoseGuzman <sjm.guzman@gmail.com>
2022-10-05 19:09:11 +0200

    correct javascript to simply test

--- js/mytest.js
+++ js/mytest.js
@@ -3,25 +3,26 @@ mytest.j
 Author: Jose Guzman
 
 Handles data submission from contact form  
+see detailed explanation here:
+https://medium.com/@jbesw/forms-without-servers-handling-form-submissions-with-lambda-4df9ec5fc473
 
 */
 const form = document.querySelector('form');
 
-//const submitResponse = document.querySelector('#response');
-const submitResponse = document.getElementById('response');
+const submitResponse = document.querySelector('#response');
 const formURL = 'https:// << ENTER YOUR API ENDPOINT HERE >> /Prod/submitForm';
 
 form.onsubmit = event => {
     event.preventDefault(); // prevent POST action cause there's no webserver
 
-    // Capture form data
+    // Capture form data and create JSON object
     let data = {};
     Array.from(form).map(input => (data[input.id] = input.value));
     console.log('Sending: ', JSON.stringify(data)); 
-    submitResponse.innerHTML = 'Sending...' // CHECK!
+    submitResponse.innerHTML = 'Sending...' // display user feedback before start 
 
-    // Create the AJAX request
-    var xhr = new XMLHttpRequest();
+    // Create the AJAX request (permit interact directly with user)
+    var xhr = new XMLHttpRequest(); // handle to send to API endpoint
     xhr.open(form.method, formURL, true);
     xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
