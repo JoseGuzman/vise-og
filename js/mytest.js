facddf3167e5b1a7995ec26cb463378371418487
JoseGuzman <sjm.guzman@gmail.com>
2022-10-14 19:21:20 +0200

    removing header

--- js/mytest.js
+++ js/mytest.js
@@ -41,9 +41,9 @@ form.addEventListener("submit", event => {
     // Create the AJAX request (permit interact directly with user) 
     var xhr = new XMLHttpRequest(); // handle to send to API endpoint 
     xhr.open("POST", endPoint, true);
-    xhr.setRequestHeader('Accept', 'application/json; charset=UTF-8');
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    //xhr.setRequestHeader('Accept', 'application/json; charset=UTF-8');
+    //xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    //xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
 
     // Send the collected data as JSON string
     xhr.send(JSON.stringify(data));
