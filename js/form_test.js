6c23f3805d580d66cdbb0275b058bb982e9a241b
JoseGuzman <sjm.guzman@gmail.com>
2022-10-17 16:25:12 +0200

    set Access-Control-Allow-Origin to *

--- js/form_test.js
+++ js/form_test.js
@@ -32,7 +32,7 @@ form.addEventListener("submit", event => {
     xhr.open("POST", endPoint, true);
     //xhr.setRequestHeader('Accept', 'application/json; charset=UTF-8');
     //xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    //xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
 
     // Send the collected data as JSON string
     xhr.send(JSON.stringify(data));
