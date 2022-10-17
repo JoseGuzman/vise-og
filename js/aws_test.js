1e723d7c142cc6e9df360bd983b81adc01b81516
JoseGuzman <sjm.guzman@gmail.com>
2022-10-17 14:19:43 +0200

    Trying with message

--- js/aws_test.js
+++ js/aws_test.js
@@ -1,21 +1,21 @@
 /*
-aws_test.js
-Author: Jose Guzman
-
-Handles data submission from a button to replace it with a JavaScript handler
-that will submit the post data and update the user interface. We post the form
-data to APT Gateway and use asynchronous JavaScript (AJAX) to provide feedback
-dinamically on the front end.
-
+* aws_test.js
+* Author: Jose Guzman
+* 
+* Handles data submission from a button to replace it with a JavaScript handler
+* that will submit the post data and update the user interface. We post the form
+* data to APT Gateway and use asynchronous JavaScript (AJAX) to provide feedback
+* dinamically on the front end.
+* 
 */
 console.log("loading aws_test.js");
 const endPoint = 'https://fuuzzxm0gh.execute-api.eu-central-1.amazonaws.com/default/myAWSbutton'
 
 const mybutton = document.querySelector("button");
 
-/* getData()
-run lambda function 
-    and prints its output in the field of the document
+/* 
+* getData()
+* run lambda function and prints its output in the field of the document
 */
 async function getData(){
     const res = await fetch(endPoint);
