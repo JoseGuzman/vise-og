7add4d3436ca6bc2644dc70d0166ad460183f18c
JoseGuzman <sjm.guzman@gmail.com>
2022-10-17 18:26:05 +0200

    testing setRequestHeader

--- js/form_test.js
+++ js/form_test.js
@@ -30,12 +30,15 @@ form.addEventListener("submit", event => {
     // Create the AJAX request (permit interact directly with user) 
     var xhr = new XMLHttpRequest(); // handle to send to API endpoint 
     xhr.open("POST", endPoint, true);
+    xhr.setRequestHeader("X-PINGOTHER", "pingpong");
+    xhr.setRequestHeader("Content-Type", "text/xml");
     //xhr.setRequestHeader('Accept', 'application/json; charset=UTF-8');
     //xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    //xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 
     // Send the collected data as JSON string
-    xhr.send(JSON.stringify(data));
+    //xhr.send(JSON.stringify(data));
+    xhr.send("<person><name>Jose</name></person>")
 
     const formResponse = document.getElementById("lambda_text");
     xhr.onloadend = response => {
